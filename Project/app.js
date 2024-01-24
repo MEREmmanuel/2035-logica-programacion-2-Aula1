e5678ff19d82492ecd9522b8cffe2d6266a8f094
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-23 20:14:23 -0600

    add tittle & paragraph and init onClick event on button Intentar

---
+++ Project/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'Guess de number!';
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'Enter a number from 1 to 10:'
+
+function userAttempt(){
+    alert('Function is work!')
+}
